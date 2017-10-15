77499ef4f3f319047936b349b21e65590af35f33
Hafsteinn Stefánsson <hafsteinns16@ru.is>
2017-10-15 03:21:45 +0000

    refactoring: added functions toInt and sum

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -9,13 +9,22 @@ public class Calculator{
 		else{
 			if(text.contains(",")){
 				String numbers[] = text.split(",");
-				int result = 0;
-				for(String number : numbers){
-					result += Integer.parseInt(number);
-				}
-				return result;				
+				return sum(numbers);	
 			}
 			return Integer.parseInt(text);
 		}
+	}
+
+
+	private static int toInt(String number){
+		return Integer.parseInt(number);
+	}
+
+	private static int sum(String [] numbers){
+		int result = 0;
+		for(String number : numbers){
+			result += toInt(number);
+		}
+		return result;
 	}
 }
